333b6b063e1a215a310b34dd0de230cbcfcd0266
jay <traeper@gmail.com>
2018-12-29 10:51:34 +0900

    commit

---
+++ lesson/lesson10/sort.py
@@ -0,0 +1,21 @@
+#
+# Example) merge one sheet
+#
+import pandas as pd
+
+df = pd.read_excel('b.xlsx')
+
+print("before sorting")
+print(df)
+
+print("\nafter sorting")
+sorted_df = df.sort_values(by=['payment'], ascending=False)
+print(sorted_df)
+
+print("\nslicing sorted data frame")
+print(sorted_df[0:4])
+
+print("\nsummation payments")
+print(sorted_df[0:4]['payment'].sum())
+
+
